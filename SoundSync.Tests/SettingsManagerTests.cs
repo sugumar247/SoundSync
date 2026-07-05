9c859f49d6338cabcca5b7a451dd0e0ecf617a34
Sugumar S <sugumar2210801@ssn.edu.in>
2026-07-05 17:45:56 +0530

    test: add unit test project structure and placeholder test

---
+++ SoundSync.Tests/SettingsManagerTests.cs
@@ -0,0 +1,13 @@
+using Xunit;
+
+namespace SoundSync.Tests
+{
+    public class SettingsManagerTests
+    {
+        [Fact]
+        public void FailingPlaceholderTest()
+        {
+            Assert.True(true);
+        }
+    }
+}
